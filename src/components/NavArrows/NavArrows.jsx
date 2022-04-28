c17dad68470e261b3f8c86841c254740d48d93d7
galanR <73245438+sgalanb@users.noreply.github.com>
2022-04-28 10:09:52 -0300

    MODIFY: require to add at least 1 poap

--- src/components/NavArrows/NavArrows.jsx
+++ src/components/NavArrows/NavArrows.jsx
@@ -3,13 +3,13 @@ import './NavArrows.css'
 import { Link } from 'wouter';
 import { ReactComponent as LeftArrow } from '../../images/arrow-solid.svg'
 
-export default function NavArrows({backPath, nextPath}) {
+export default function NavArrows({backPath, nextPath, rightArrowFunction = () => {}, leftArrowFunction = () => {}}) {
 
-    const left_arrow =  <Link href={backPath}>
+    const left_arrow =  <Link onClick={leftArrowFunction} href={backPath}>
                             <LeftArrow className='left-arrow' fill='#fff' width={'30px'} />
                         </Link>
 
-    const right_arrow = <Link className='right-arrow-link' href={nextPath}>
+    const right_arrow = <Link onClick={rightArrowFunction} className='right-arrow-link' href={nextPath}>
                             <LeftArrow className='right-arrow' fill='#fff' width={'30px'} />
                         </Link>
 
--- src/pages/2-Events/Events.js
+++ src/pages/2-Events/Events.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './Events.css'
 import Navbar from '../../containers/Navbar/Navbar';
 import IdInput from '../../components/IdInput/IdInput'
 import AddedEvents from '../../containers/AddedEvents/AddedEvents';
 import Heading2 from '../../components/Heading2/Heading2';
 import NavArrows from '../../components/NavArrows/NavArrows';
+import IdContext from '../../context/IdContext';
 
 export default function Events() {
+    const { state } = useContext(IdContext)
+
     return (
       <div className='eventsContainer'>
         <Navbar />
-        <NavArrows nextPath={'number-of-winners'} />
+        <NavArrows nextPath={state.listOfIDs.length == 0 ? '/poaps' : 'number-of-winners'} rightArrowFunction={() => {
+          if (state.listOfIDs.length == 0) {alert('Add at least 1 event')}
+        }} />
         <Heading2 text={'Add POAPs to the raffle'} />
         <IdInput />
         <AddedEvents />
