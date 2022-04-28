343b47e5b0db41b705c54ed1b0092cc253d3ee00
galanR <73245438+sgalanb@users.noreply.github.com>
2022-04-27 22:24:21 -0300

    MODIFY: improve code readability

--- src/containers/WinnersList/WinnersList.jsx
+++ src/containers/WinnersList/WinnersList.jsx
@@ -8,10 +8,6 @@ export default function WinnersList() {
     const [isLoading, setIsLoading ] = useState(true)
     const [results, setResults ] = useState()
 
-/*     const winnersArrayPlaceholder = ['0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B', 'santigalan.eth', '0x21000000A991163FE61028D3d05dD966b25cA7e9'] */
-
-/* remove participantsArray[getWinner] from participantsArray (with .splice())  */
-
     useEffect(() => {
         setTimeout(() => {
             setResults(winnersArray.map(address =><SingleWinner key={winnersArray.indexOf(address)} number={winnersArray.indexOf(address) + 1} address={address}/> ))
@@ -21,9 +17,6 @@ export default function WinnersList() {
         }, 5000);
     }, [])
     
-
-    
-
     return (
         <div className='winnersListContainer'>
             {isLoading ? 'Loading...' : results}
--- src/hooks/useGetParticipants.jsx
+++ src/hooks/useGetParticipants.jsx
@@ -7,14 +7,14 @@ const useGetParticipants = () => {
     state.listOfIDs.forEach(id => {
         const apiURL = `https://api.poap.xyz/event/${id}/poaps?limit=300&offset=0`
         fetch(apiURL)
-                .then(response => response.json())
-                .then(response => {
-                    const tokensArray = response.tokens
-                    const addressesArray = tokensArray.map(token => token.owner.id)
-                    addressesArray.forEach(address => finalArray.push(address))
-                })
-                .catch((error) => {
-                    console.log(`Fetch error: ${error.message}`)
+            .then(response => response.json())
+            .then(response => {
+                const tokensArray = response.tokens
+                const addressesArray = tokensArray.map(token => token.owner.id)
+                addressesArray.forEach(address => finalArray.push(address))
+            })
+            .catch((error) => {
+                console.log(`Fetch error: ${error.message}`)
         })
     })
     return finalArray
