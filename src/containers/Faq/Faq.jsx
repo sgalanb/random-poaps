4dcf04758f5710a310ffa0d031beb336d643a357
galanR <73245438+sgalanb@users.noreply.github.com>
2022-05-10 16:55:28 -0300

    add faq content & styles

--- src/containers/Faq/Faq.jsx
+++ src/containers/Faq/Faq.jsx
@@ -9,9 +9,21 @@ export default function Faq() {
     return (
         <div className={`background ${state.faqActive ? 'active' : 'disabled'}`} onClick={() => {setFaqActive(false)}}>
             <div className='faqContainer' onClick={handleContainerClick}>
+
+                <p className='close' onClick={()=>setFaqActive(false)}>X</p>
                 <h2 className='faq-title'>F.A.Q.</h2>
-                <h3 className='ask'>🔽 How do winners prove the ownership of their address?</h3>
-                <p className='response'>Winners can sign a message with their address at <a target="_blank" rel="noopener noreferrer" href="https://app.mycrypto.com/sign-message">this site</a>. Organizers can verify the signature <a target="_blank" rel="noopener noreferrer" href="https://app.mycrypto.com/verify-message">here.</a> </p>
+
+                <div className='questions-container'>
+                    <h3 className='ask'>🔽 Where can I find events IDs?</h3>
+                    <p className='response'>You can browse all poaps and their IDs at the <a target="_blank" rel="noopener noreferrer" href="https://poap.gallery/">official galery</a>.</p>
+
+                    <h3 className='ask'>🔽 How do winners prove the ownership of their address?</h3>
+                    <p className='response'>Winners can sign a message with their address at <a target="_blank" rel="noopener noreferrer" href="https://app.mycrypto.com/sign-message">this site</a>. Organizers can verify the signature <a target="_blank" rel="noopener noreferrer" href="https://app.mycrypto.com/verify-message">here.</a> </p>
+
+                    <h3 className='ask'>🔽 Is there any technical limitation?</h3>
+                    <p className='response'> The only limitation is that events with a supply of +300 poaps are not supported (yet).</p>
+                </div>
+
             </div>
         </div>
     )
