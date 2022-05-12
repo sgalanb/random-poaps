9d051d322b29cd91ecfd8ed96dc5a4d75038d724
galanR <73245438+sgalanb@users.noreply.github.com>
2022-05-12 14:54:32 -0300

    add twitter card metadata

--- src/pages/1-Home/Home.js
+++ src/pages/1-Home/Home.js
@@ -10,6 +10,11 @@ export default function Home() {
             <Helmet>
                 <title>Home | Random POAPs</title>
                 <meta name="description" content="Create raffles between POAP token holders. No sign up required." />
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:site" content="@sgalanb" />
+                <meta name="twitter:title" content="Random POAPs" />
+                <meta name="twitter:description" content="Create raffles between POAP token holders. No sign up required." />
+                <meta name="twitter:image" content="https://imgur.com/a/oxiYw5y" />
             </Helmet>
 
             <Navbar type='social' />
