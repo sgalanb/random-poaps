d303de2844836ea814d0c9ca8ca1a0af07de662c
galanR <73245438+sgalanb@users.noreply.github.com>
2022-05-14 20:10:37 -0300

    add pagination to enable events with +300 poaps

--- src/services/getTotalAddresses.js
+++ src/services/getTotalAddresses.js
@@ -1,22 +1,37 @@
 export const getTotalAddresses = (ids) => {
-    function fetchAll() {
-      const urls = []
-      for (const id of ids) {
-        const templateUrl = fetch(`https://api.poap.xyz/event/${id}/poaps?limit=300&offset=0`)
-        urls.push(templateUrl)
-      }
-      return Promise.all(urls)
+    async function fetchAll() {
+        const urls = []
+        for (const id of ids) {
+            await fetch(`https://api.poap.xyz/event/${id}/poaps`)
+                .then(response => {
+                    if (response.ok) { return response.json() }
+                    else { throw new Error(response.status) }  
+                })
+                .then(response => {
+                    const apiLimit = 300
+                    const numberOfRequests = parseInt(response.total / apiLimit) + 1
+
+                    let i = 1
+                    let offset = 0
+                    while (i <= numberOfRequests) {
+                        urls.push(fetch(`https://api.poap.xyz/event/${id}/poaps?limit=300&offset=${offset}`))
+                        offset = offset + 300
+                        i++
+                    }
+                })
+        }
+        return Promise.all(urls)
     }
   
     const json = (response) =>
-      Promise.all(
-        response.map((response) => {
-          if (response.ok) { return response.json() }
-          else { throw new Error(response.statusCode) }
-        })
-      )
-    
-    const mapArrayOfResponses = (responses) => responses.map((object) => object.tokens.map((token) => token.owner.id))
+        Promise.all(
+            response.map((response) => {
+               if (response.ok) { return response.json() }
+                else { throw new Error(response.statusCode) }
+            })
+        )
+
+    const mapArrayOfResponses = (response) => response.map((object) => object.tokens.map((token) => token.owner.id))
 
     const concatArrays = (response) => response.reduce((acc, av) => acc.concat(av), [])
   
