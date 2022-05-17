e507cb6c9e46ebcd19998d6166f786fd03d95911
galanR <73245438+sgalanb@users.noreply.github.com>
2022-05-16 23:56:04 -0300

    add question to FAQ

--- src/containers/Faq/Faq.jsx
+++ src/containers/Faq/Faq.jsx
@@ -14,10 +14,14 @@ export default function Faq() {
 
                 <div className='questions-container'>
                     <h3 className='ask'>🔽 Where can I find events IDs?</h3>
-                    <p className='response'>You can find all the created events and their IDs in the <a target="_blank" rel="noopener noreferrer" href="https://poap.gallery/">official gallery</a>.</p>
+                    <p className='response'>You can find all the created events and their IDs at <a target="_blank" rel="noopener noreferrer" href="https://poap.gallery/">poap.gallery</a>.</p>
 
                     <h3 className='ask'>🔽 How can winners prove ownership of their address?</h3>
                     <p className='response'>Winners can sign a message with their address at <a target="_blank" rel="noopener noreferrer" href="https://app.mycrypto.com/sign-message">this site</a>. Organizers can verify the signature <a target="_blank" rel="noopener noreferrer" href="https://app.mycrypto.com/verify-message">here.</a> </p>
+
+                    <h3 className='ask'>🔽 What&apos;s the difference with poap.fun?</h3>
+                    <p className='response'> The main difference is that people don&apos;t have to sign up for participate in the raffles.
+                    This has some disadvantages, like that raffles results are&apos;t stored anywhere, but it&apos;s perfect for quick hassle free raffles. </p>
 
                     <h3 className='ask'>🔽 Is there any limitation?</h3>
                     <p className='response'> Events with a supply of +5000 poaps are not supported due to POAP API limitations.</p>
