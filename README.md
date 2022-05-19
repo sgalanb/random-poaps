44d66d8c2c0dd77dbfdb8e2799aba43f38db85e7
galanR <73245438+sgalanb@users.noreply.github.com>
2022-05-19 00:33:21 -0300

    add copy to clipboard & modify readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Random POAPs
 Create raffles between POAP token holders. No participants sign up required.
-
-## Backlog
-- Add a filter to only allow addresses/ENS domains that reply to a certain tweet participate in a raffle.
+##  How it works?
+ 1. Create a new raffle.
+ 2. Add the ID of the events that you want to be part of the raffle.
+ 3. Select the number of winners.
+ 4. Start the raffle and wait for the results.
--- src/components/SingleWinner/SingleWinner.jsx
+++ src/components/SingleWinner/SingleWinner.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './SingleWinner.css'
 
+import { ReactComponent as CopySVG } from 'images/copy-solid.svg'
+
 export default function SingleWinner({address, number}) {
 
     return (
         <div className='singleWinnerContainer'>
             <p className='winner-number'>{number}.</p>
             <a className='winner-address' href={`https://welook.io/${address}`} target="_blank" rel="noopener noreferrer">{address}</a>
+            <CopySVG title='Copy to clipboard' className='winner-copy' width={'20px'} onClick={() =>{
+                navigator.clipboard.writeText(address)
+                }} />
         </div>        
     )
 }
