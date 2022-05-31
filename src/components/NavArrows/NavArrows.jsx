bfeb38b4504a9020c3964a602c0fbf390f59fc1b
galanR <73245438+sgalanb@users.noreply.github.com>
2022-05-30 21:32:02 -0300

    fix arrow svg import name

--- src/components/NavArrows/NavArrows.jsx
+++ src/components/NavArrows/NavArrows.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { Link } from 'wouter';
 import './NavArrows.css'
 
-import { ReactComponent as LeftArrow } from 'images/arrow-solid.svg'
+import { ReactComponent as Arrow } from 'images/arrow-solid.svg'
 
 export default function NavArrows({backPath, nextPath, rightArrowFunction = () => {}, leftArrowFunction = () => {}}) {
 
     const left_arrow =  <Link onClick={leftArrowFunction} href={backPath}>
-                            <LeftArrow className='left-arrow' fill='#fff' width={'30px'} />
+                            <Arrow className='left-arrow' fill='#fff' width={'30px'} />
                         </Link>
 
     const right_arrow = <Link onClick={rightArrowFunction} className='right-arrow-link' href={nextPath}>
-                            <LeftArrow className='right-arrow' fill='#fff' width={'30px'} />
+                            <Arrow className='right-arrow' fill='#fff' width={'30px'} />
                         </Link>
 
     if (nextPath == undefined) {
